e3588df421607de087a0017ccf47558e49166dc7
Syed Ekram Uddin <ekram.syed@gmail.com>
2019-12-04 08:44:01 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar />
+      <Sidebar />
     </div>
   );
 }
